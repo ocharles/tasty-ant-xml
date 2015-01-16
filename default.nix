aff036a373092a0342413303a487c9564e35212c
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 20:10:05 +0000

    Regenerate nix expressions

--- default.nix
+++ default.nix
@@ -1,18 +1,15 @@
-{ cabal, genericDeriving, mtl, stm, tagged, tasty, transformers
-, xml
+{ mkDerivation, base, containers, generic-deriving, ghc-prim, mtl
+, stdenv, stm, tagged, tasty, transformers, xml
 }:
-
-cabal.mkDerivation (self: {
+mkDerivation {
   pname = "tasty-ant-xml";
   version = "1.0.1";
   src = ./.;
   buildDepends = [
-    genericDeriving mtl stm tagged tasty transformers xml
+    base containers generic-deriving ghc-prim mtl stm tagged tasty
+    transformers xml
   ];
-  meta = {
-    homepage = "http://github.com/ocharles/tasty-ant-xml";
-    description = "Render tasty output to XML for Jenkins";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  homepage = "http://github.com/ocharles/tasty-ant-xml";
+  description = "Render tasty output to XML for Jenkins";
+  license = stdenv.lib.licenses.bsd3;
+}
